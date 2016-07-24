cc37c48d5d326f7708dd73952147eccb5ecc27a8
ZachR <zachary.nicholas.robinson@gmail.com>
2016-07-24 17:33:28 -0400

    Initial Commit

---
+++ mangaRoulette.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+		
+class mangaRoulette(App):
+    def build(self):      
+	return
+
+mangaRoulette().run()
