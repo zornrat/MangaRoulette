fa0ba44e8569e5af2732deb0ba16bde51c11d14f
ZachR <zachary.nicholas.robinson@gmail.com>
2016-07-25 23:06:14 -0400

    Fully functional, just needs a bit of aesthetic imporvement

--- mList.py
+++ mList.py
@@ -7,117 +7,177 @@ import httplib
 import json
 import random
 import urllib
-import sys
 import getopt
 import datetime
 import webbrowser
+import scipy
+from scipy.stats import scoreatpercentile
+from kivy.uix.widget import Widget
+import threading
+from kivy.lang import Builder
 
 
-def mList(argv):
-    genreList = ["Action", "Adult", "Adventure", "Comedy", "Doujinshi", "Drama", "Ecchi", "Fantasy", "Gender Bender",
-                 "Harem", "Historical", "Horror", "Josei", "Martial Arts", "Mature", "Mecha", "Mystery", "One Shot",
-                 "Psychological", "Romance", "School Life", "Sci-fi", "Seinen", "Shoujo", "Shounen", "Slice of Life",
-                 "Smut", "Sports", "Supernatural", "Tragedy", "Webtoons", "Yaoi", "Yuri"]
+class RootWidget(Widget):
+    genreStatus = {}
 
-    try:
-        opts, args = getopt.getopt(argv, "g:s:b:a:m:f:",
-                                   ["genre=", "status=", "before=", "after=", "more-hits=", "fewer-hits="])
-    except getopt.GetoptError:
-        sys.stderr.write('Invalid argument usage\n')
-        sys.exit(1)
+    def spin(self):
 
-    genres = []
-    genreFlag = 0
-    statusFlag = 0
-    beforeFlag = 0
-    afterFlag = 0
-    mhFlag = 0
-    fhFlag = 0
+        args = []
 
-    for opt, arg in opts:
-        if opt in ("-g", "--genre"):
-            if (arg not in genreList):
-                sys.stdout.write(
-                    arg + " is not a valid genre. " + "Genres are: " + '["Action","Adult","Adventure","Comedy","Doujinshi","Drama","Ecchi","Fantasy","Gender Bender","Harem","Historical","Horror","Josei","Martial Arts","Mature","Mecha","Mystery","One Shot","Psychological","Romance","School Life","Sci-fi","Seinen","Shoujo","Shounen","Slice of Life","Smut","Sports","Supernatural","Tragedy","Webtoons","Yaoi","Yuri"]' + "\n")
-                sys.exit(0)
-            genres.append(unicode(arg))
-            genreFlag = 1
-        elif opt in ("-s", "--status"):
-            statusFlag = 1;
-            status = int(arg)
-            if (status != 1 and status != 2):
-                sys.stderr.write('invalid status: should be 1 for "ongoing" or 2 for "completed"\n')
-                sys.exit(1)
-        elif opt in ("-b", "--before"):
-            beforeFlag = 1
-            beforeDate = datetime.date.fromtimestamp(float(arg))
-        elif opt in ("-a", "--after"):
-            afterFlag = 1
-            afterDate = datetime.date.fromtimestamp(float(arg))
-        elif opt in ("-m", "--more-hits"):
-            mhFlag = 1
-            moreHitsThan = int(arg)
-        elif opt in ("-f", "--fewer-hits"):
-            fhFlag = 1
-            fewerHitsThan = int(arg)
+        for genre in self.genreStatus.keys():
+            if self.genreStatus[genre] == 'down':
+                args.append("--genre")
+                args.append(genre)
+
+        if self.ids.statusSpinner.text == 'Ongoing':
+            args.append("--status")
+            args.append("1")
+        elif self.ids.statusSpinner.text == 'Finished':
+            args.append("--status")
+            args.append("2")
+
+        if self.ids.popSpinner.text != 'Select Popularity':
+            args.append("--popularity")
+            args.append(self.ids.popSpinner.text)
+
+        self.ids.startSpin.font_size = 72
+        self.ids.startSpin.text = 'Working...'
+
+        spinThread = threading.Thread(target=self.mList, args=[args])
+        spinThread.start()
+
+        return self
+
+    def spinEnd(self, message='\0'):
+
+        if message != '\0':
+            self.ids.startSpin.font_size = 30
+            self.ids.startSpin.text = message
         else:
-            sys.stderr.write("Misc arg error\n")
-            sys.exit(1)
+            self.ids.startSpin.font_size = 72
+            self.ids.startSpin.text = 'Spin'
 
-    try:
-        conn = httplib.HTTPSConnection("www.mangaeden.com")
-        conn.request("GET", "/api/list/0/")
-        r1 = conn.getresponse()
-    except httplib.HTTPException:
-        sys.stderr.write('Failed http connection: Mangaeden might be down or you may not have internet connectivity\n')
-        sys.exit(1)
+        return self
 
-    manList = json.loads(r1.read())
-    yList = []
-    for entries in manList['manga']:
-        addit = 1
+    def mList(self, argv=[]):
 
-        if (genreFlag):
-            for g in genres:
-                if (g not in entries['c']):
-                    addit = 0
+        genreList = ["Action", "Adult", "Adventure", "Comedy", "Doujinshi", "Drama", "Ecchi", "Fantasy",
+                     "Gender Bender",
+                     "Harem", "Historical", "Horror", "Josei", "Martial Arts", "Mature", "Mecha", "Mystery", "One Shot",
+                     "Psychological", "Romance", "School Life", "Sci-fi", "Seinen", "Shoujo", "Shounen",
+                     "Slice of Life",
+                     "Smut", "Sports", "Supernatural", "Tragedy", "Webtoons", "Yaoi", "Yuri"]
 
-        if (statusFlag):
-            if (status != entries['s']):
-                addit = 0
-
-        if (beforeFlag):
-            if (not entries.has_key('ld')):
-                addit = 0
-            else:
-                if (datetime.date.fromtimestamp(entries['ld']) > beforeDate):
-                    addit = 0
-
-        if (afterFlag):
-            if (not entries.has_key('ld')):
-                addit = 0
-            else:
-                if (datetime.date.fromtimestamp(entries['ld']) < afterDate):
-                    addit = 0
-
-        if (mhFlag):
-            if (moreHitsThan >= entries['h']):
-                addit = 0
-
-        if (fhFlag):
-            if (fewerHitsThan <= entries['h']):
-                addit = 0
-
-        if (addit == 1):
-            yList.append(entries['a'])
-
-    if (yList):
         try:
-            webbrowser.open_new_tab("http://www.mangaeden.com/en/en-manga/" + urllib.quote_plus(random.choice(yList)))
+            opts, args = getopt.getopt(argv[0:], "g:s:b:a:p:",
+                                       ["genre=", "status=", "before=", "after=", "popularity="])
+        except getopt.GetoptError:
+            raise Exception('Invalid argument usage\n')
 
-        except(webbrowser.Error):
-            sys.stderr.write("Browser Control Error: failed to open new browser tab\n")
-            sys.exit(1)
+        genres = []
+        genreFlag = 0
+        statusFlag = 0
+        beforeFlag = 0
+        afterFlag = 0
+        popFlag = 0
+        popSetting = ''
 
-    else:
-        sys.stdout.write("No Manga matching your parameters\n")
+        for opt, arg in opts:
+            if opt in ("-g", "--genre"):
+                if arg not in genreList:
+                    raise Exception(
+                        arg + " is not a valid genre. " + "Genres are: " + '["Action","Adult","Adventure","Comedy","Doujinshi","Drama","Ecchi","Fantasy","Gender Bender","Harem","Historical","Horror","Josei","Martial Arts","Mature","Mecha","Mystery","One Shot","Psychological","Romance","School Life","Sci-fi","Seinen","Shoujo","Shounen","Slice of Life","Smut","Sports","Supernatural","Tragedy","Webtoons","Yaoi","Yuri"]' + "\n")
+                genres.append(unicode(arg))
+                genreFlag = 1
+            elif opt in ("-s", "--status"):
+                statusFlag = 1
+                status = int(arg)
+                if status != 1 and status != 2:
+                    raise Exception('invalid status: should be 1 for "ongoing" or 2 for "completed"\n')
+            elif opt in ("-b", "--before"):
+                beforeFlag = 1
+                beforeDate = datetime.date.fromtimestamp(float(arg))
+            elif opt in ("-a", "--after"):
+                afterFlag = 1
+                afterDate = datetime.date.fromtimestamp(float(arg))
+            elif opt in ("-p", "--popularity"):
+                popFlag = 1
+                if arg not in ("Low", "Medium", "High"):
+                    raise Exception("Must select 'low', 'medium', or 'high' for popularity option.\n")
+                else:
+                    popSetting = arg
+            else:
+                raise Exception("Misc arg error\n")
+
+        try:
+            conn = httplib.HTTPSConnection("www.mangaeden.com")
+            conn.request("GET", "/api/list/0/")
+            r1 = conn.getresponse()
+        except httplib.HTTPException:
+            raise Exception(
+                'Failed http connection: Mangaeden might be down or you may not have internet connectivity\n')
+
+        manList = json.loads(r1.read())
+        yList = []
+
+        if popFlag == 1:
+            popArray = []
+            for m in manList['manga']:
+                popArray.append(int(m['h']))
+            firstThridPercentile = scipy.stats.scoreatpercentile(popArray, 33)
+            secondThirdPercentile = scipy.stats.scoreatpercentile(popArray, 67)
+
+        for entries in manList['manga']:
+            addit = 1
+
+            if genreFlag:
+                for g in genres:
+                    if g not in entries['c']:
+                        addit = 0
+
+            if statusFlag:
+                if status != entries['s']:
+                    addit = 0
+
+            if beforeFlag:
+                if not entries.has_key('ld'):
+                    addit = 0
+                else:
+                    if datetime.date.fromtimestamp(entries['ld']) > beforeDate:
+                        addit = 0
+
+            if afterFlag:
+                if not entries.has_key('ld'):
+                    addit = 0
+                else:
+                    if datetime.date.fromtimestamp(entries['ld']) < afterDate:
+                        addit = 0
+
+            if popFlag:
+                if popSetting == "Low":
+                    if entries['h'] > firstThridPercentile:
+                        addit = 0
+                elif popSetting == "Medium":
+                    if entries['h'] <= firstThridPercentile or entries['h'] >= secondThirdPercentile:
+                        addit = 0
+                elif popSetting == "High":
+                    if entries['h'] < secondThirdPercentile:
+                        addit = 0
+
+            if addit == 1:
+                yList.append(entries['a'])
+
+        if yList:
+            try:
+                webbrowser.open_new_tab(
+                    "http://www.mangaeden.com/en/en-manga/" + urllib.quote_plus(random.choice(yList)))
+                self.spinEnd()
+
+            except webbrowser.Error:
+                raise Exception("Browser Control Error: failed to open new browser tab\n")
+
+        else:
+            self.spinEnd("No Results. Try again?")
+
+
+myRoot = Builder.load_file('RootWidget.kv')
--- mangaRoulette.py
+++ mangaRoulette.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#credits: kivy
+# credits: kivy
+# Still need to comment code
+# Do texturing next
 
 import kivy
+from kivy.app import App
+from kivy.config import Config
+from mList import myRoot
+
 kivy.require('1.9.1')
 
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.togglebutton import ToggleButton
-from kivy.uix.widget import Widget
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.properties import StringProperty
-from kivy.config import Config
 
-class GenToggle(ToggleButton):
-    pass
-
-class RootWidget(Widget):
-    pass
-
-class DateTimeSelect(Widget):
-    pass
-
-class mangaRoulette(App):
+class MangaRoulette(App):
     def build(self):
-        Config.set('graphics','height','600')
-        Config.set('graphics', 'width','800')
-        Config.set('graphics','minimum_height','600')
-        Config.set('graphics', 'minimum_width','800')
-        return RootWidget()
+        Config.set('graphics', 'height', '600')
+        Config.set('graphics', 'width', '800')
+        Config.set('graphics', 'minimum_height', '600')
+        Config.set('graphics', 'minimum_width', '800')
+        return myRoot
 
-mangaRoulette().run()
+
+if __name__ == "__main__":
+    MangaRoulette().run()
