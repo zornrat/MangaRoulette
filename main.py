7b8b3c435f9c76c0f83212aaca553862b907938d
zornrat <zach@arch-zach.hsd1.mi.comcast.net>
2016-07-27 15:52:16 -0400

    Prepared android build config

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# credits: kivy
+# Still need to comment code
+# Do texturing next
+
+import kivy
+from kivy.app import App
+from kivy.config import Config
+from mList import myRoot
+
+kivy.require('1.9.1')
+
+
+class MangaRoulette(App):
+    def build(self):
+        return myRoot
+
+
+if __name__ == "__main__":
+    MangaRoulette().run()
